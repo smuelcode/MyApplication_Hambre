78b830e9ebb8fb590515619fd3b56ec1e8f7d03b
Samuel <sasa2015.sr@gmail.com>
2017-04-01 14:35:44 -0300

    2 Upload

---
+++ app/src/main/java/com/example/hambre/myapplication/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.hambre.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btncadastrar;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* FacebookSdk.sdkInitialize(getApplicationContext());
+         initializeControls();
+         loginwithFB();*/
+        btncadastrar = (Button)findViewById(R.id.N_Cadastrar);
+
+
+        btncadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent it = new Intent(getApplication(),Cadastro.class);
+                startActivity(it);
+
+            }
+        });
+
+
+    }
+}
+
+
+
+
+
+    /*TextView textStatus;
+    LoginButton login_Button;
+    CallbackManager callbackManager;
+
+    private void initializeControls(){
+        textStatus = (TextView)findViewById(R.id.textStatus);
+        login_Button = (LoginButton)findViewById(R.id.fbLogin);
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    private void loginwithFB() {
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textStatus.setText("Login Sucess\n" + loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textStatus.setText("Login Cancelled.");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textStatus.setText("Login Error: " + error.getMessage());
+
+            }
+        });
+    }
+
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
